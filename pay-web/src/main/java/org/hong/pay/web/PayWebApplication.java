feba82f09a4abc00b88ab7bae193bb472fb6c42c
hong <lun3322@126.com>
2023-07-30 17:38:11 +0800

    更新 可以启动了

---
+++ pay-web/src/main/java/org/hong/pay/web/PayWebApplication.java
@@ -0,0 +1,11 @@
+package org.hong.pay.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayWebApplication.class, args);
+    }
+}
---
+++ pay-web/src/main/java/org/hong/pay/web/controller/HomeController.java
@@ -0,0 +1,14 @@
+package org.hong.pay.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
