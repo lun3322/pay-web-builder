29cde22865ead44c6555df6f201d6c9587c3f43f
hong <lun3322@126.com>
2023-07-30 20:01:04 +0800

    更新 地址判断是否沙箱

--- pay-service/src/main/java/org/hong/pay/service/config/AliPayServiceFix.java
+++ pay-service/src/main/java/org/hong/pay/service/config/AliPayServiceFix.java
@@ -2,13 +2,17 @@ package org.hong.pay.service.config;
 
 import com.egzosn.pay.ali.api.AliPayConfigStorage;
 import com.egzosn.pay.ali.api.AliPayService;
+import com.egzosn.pay.ali.bean.AliPayConst;
 import com.egzosn.pay.common.http.HttpConfigStorage;
+
+import static com.egzosn.pay.ali.bean.AliPayConst.HTTPS_REQ_URL;
 
 public class AliPayServiceFix extends AliPayService {
 
     @Override
     public String getReqUrl() {
-        return "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
+        final String https_sandbox_url = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
+        return payConfigStorage.isTest() ? https_sandbox_url : HTTPS_REQ_URL;
     }
 
     public AliPayServiceFix(AliPayConfigStorage payConfigStorage, HttpConfigStorage configStorage) {
