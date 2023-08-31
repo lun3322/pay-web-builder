05cedf783ae8183440949f291af599d81248a500
hong <lun3322@126.com>
2023-08-31 17:12:56 +0800

    重构 使用spring cloud config

--- pay-service/src/main/java/org/hong/pay/service/config/AliPayIJPayConfig.java
+++ pay-service/src/main/java/org/hong/pay/service/config/AliPayIJPayConfig.java
@@ -1,25 +1,31 @@
 package org.hong.pay.service.config;
 
+import cn.hutool.core.net.url.UrlBuilder;
+import cn.hutool.http.HttpUtil;
 import com.alipay.api.AlipayApiException;
 import com.ijpay.alipay.AliPayApiConfig;
 import com.ijpay.alipay.AliPayApiConfigKit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.config.client.ConfigClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class AliPayIJPayConfig {
     private final AlipayProperties alipayProperties;
-
+    private final ConfigClientProperties configClientProperties;
+    private final Environment environment;
     @Value("${spring.application.name}")
     private String appName;
 
     @Bean
     public AliPayApiConfig aliPayApiConfig() throws AlipayApiException {
+        log.info(configClientProperties.toString());
         AliPayApiConfig result = AliPayApiConfig.builder();
 
         final String https_sandbox_url = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
@@ -31,7 +37,7 @@ public class AliPayIJPayConfig {
         result.setSignType("RSA2");
         result.setAliPayCertContent(getCert(alipayProperties.getAliPayCert()));
         result.setAppCertContent(getCert(alipayProperties.getMerchantCert()));
-        result.setAliPayRootCertContent(getCert((alipayProperties.getAliPayRootCert())));
+        result.setAliPayRootCertContent(getCert(alipayProperties.getAliPayRootCert()));
         result.buildByCertContent();
 
         AliPayApiConfigKit.putApiConfig(result);
@@ -39,6 +45,14 @@ public class AliPayIJPayConfig {
     }
 
     private String getCert(String name) {
-        return name;
+        String url = UrlBuilder.of(configClientProperties.getUri()[0])
+                .addPath(appName)
+                .addPath(environment.getActiveProfiles()[0])
+                .addPath(configClientProperties.getLabel())
+                .addPath(name)
+                .toString();
+
+        log.info(url);
+        return HttpUtil.get(url);
     }
 }
--- pay-web/src/main/java/org/hong/pay/web/controller/HomeController.java
+++ pay-web/src/main/java/org/hong/pay/web/controller/HomeController.java
@@ -1,8 +1,5 @@
 package org.hong.pay.web.controller;
 
-import com.tencent.cloud.polaris.context.config.PolarisContextProperties;
-import com.tencent.polaris.configuration.api.core.ConfigFile;
-import com.tencent.polaris.configuration.api.core.ConfigFileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class HomeController {
-    private final ConfigFileService configFileService;
-    private final PolarisContextProperties polarisContextProperties;
-
     @GetMapping
     public String home() {
         return "home";
-    }
-
-    @GetMapping("config")
-    public String config() {
-        ConfigFile alipay = configFileService.getConfigFile(polarisContextProperties.getNamespace(), "koude-pay-alipay", "alipay/alipayPublicCert.crt");
-        return alipay.getContent();
     }
 }
